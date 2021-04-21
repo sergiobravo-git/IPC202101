45e83c9c922af6f60b0d7200fdb9b434341567ee
Sergio Gomez <sergiobravo@hotmail.com>
2021-04-21 15:26:18 -0600

    IPC 21 de abril 2021 15:26pm

--- arch2.cpp
+++ arch2.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
  
 int main(int argc, char** argv)
 {
 	FILE *fp;  // f file p pointe o apuntador
-	fp = fopen ( "fichero.in", "r" );        
+	char nombre_archivo[100];
+	printf("Ingrese el nombre del archivo:");
+	gets(nombre_archivo);
+	fp = fopen ( nombre_archivo, "r" );        
 	if (fp==NULL) {fputs ("File error",stderr); exit (1);}
 	fclose ( fp );
 
