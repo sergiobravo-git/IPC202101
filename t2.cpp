577fae19e241fccf82e28848ae23de8912e08124
sergiobravogit <sergiobravo@hotmail.com>
2021-05-03 15:59:37 -0600

    3 mayo 2021 3:58 pm incluyendo un ejemplo con un constructor

---
+++ t2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+// Esto define la clase CRender
+class CTrianguloPascal {
+
+	int  nivel;
+	
+	int factorial(int x)
+	{
+		if (x==0)
+		return 1;
+		else x * factorial(x-1);
+	}
+	
+	int combinacion(int m, int n)
+	{
+		return factorial(m)/(factorial(m)*factorial(m-n));
+	}
+
+public:
+	CTrianguloPascal()
+	{
+	}
+	CTrianguloPascal(int entradanivel)
+	{
+		nivel = entradanivel;
+	}
+	void dibujar()
+	{
+		for(int filas=0;filas<=nivel;filas++)
+		{
+			for(int columnas=0;columnas<=filas;columnas++)
+				printf("%5d",combinacion(filas,columnas));
+			printf("\n");
+		}
+	}
+	int getNivel()
+	{
+		return nivel;	
+	}
+	void setNivel(int entrada)
+	{
+		nivel = entrada;	
+	}	
+};
+
+int main (int argc, char **argv){
+    // crear 2 objetos CRender
+    CTrianguloPascal t1(3), t2;
+
+	
+	printf("Estamos dibujando el triángulo de pascal 1\n");
+	t1.dibujar();
+	
+	t2.setNivel(5);
+	
+	printf("Estamos dibujando el triángulo de pascal 2");
+	t2.dibujar();
+
+}
